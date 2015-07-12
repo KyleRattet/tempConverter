9e38c1a58a8f5726f1918e8e29bda95bb9a81f48
KyleRattet <krattet@gmail.com>
2015-07-12 14:04:02 -0600

    basic C to F conversion

---
+++ main.js
@@ -0,0 +1,9 @@
+//Temperature Convertor Write code that asks the user for a temperature in Celsius and converts it to Fahrenheit. Bonus: ask the user first if they want to convert from F to C or C to F.
+
+
+//Temperature from Celcisu to Fahrenheit
+var temperature = prompt ("Please enter a temperature in Celsius to be converted to Fahrenheit");
+
+tempCelcius = temperature * (9/5) + 32;
+
+alert ("Your Celsius temperature equates to "+tempCelcius+"\xB0F");
